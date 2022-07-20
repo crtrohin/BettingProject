6a71d909155dfe4343a5a9f86953811b3a431611
Cristina Trohin <cr.trohin@gmail.com>
2022-07-20 14:30:22 +0300

    Cleanup

--- src/main/java/com/codefactorygroup/betting/exception/NoSuchParticipantExistsException.java
+++ src/main/java/com/codefactorygroup/betting/exception/NoSuchParticipantExistsException.java
@@ -1,7 +1,6 @@
 package com.codefactorygroup.betting.exception;
 
 public class NoSuchParticipantExistsException extends RuntimeException {
-
     public NoSuchParticipantExistsException(Integer participantId) {
         super(String.format("No participant with ID = %d was found.", participantId));
     }
